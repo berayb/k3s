ee783d03949974c65e79429a85790e13791c33e5
Beray Bentesen <beray@qubitro.com>
2020-04-02 03:05:08 +0300

    init golang

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func init() {
@@ -10,4 +14,21 @@ func init() {
 
 func main() {
 	fmt.Println("Entered in main")
+	initRouter()
+
+}
+
+func initRouter() {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/demo", demo)
+
+	fmt.Println(http.ListenAndServe(":8000", router))
+
+}
+func demo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Demo OK")
+	json.NewEncoder(w).Encode("Demo Json OK")
+	w.WriteHeader(http.StatusOK)
 }
